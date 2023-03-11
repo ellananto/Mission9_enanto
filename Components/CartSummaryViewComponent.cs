9bb13072b48647b87ae7ec530b1e83e6126b6adf
ellananto <ellananto9@gmail.com>
2023-03-11 01:59:40 -0700

    Bootstrap Requirements

---
+++ Components/CartSummaryViewComponent.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Mission9.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Mission9.Components
+{
+    public class CartSummaryViewComponent : ViewComponent
+    {
+        private Basket basket;
+        public CartSummaryViewComponent (Basket cartService)
+        {
+            basket = cartService;
+        }
+        public IViewComponentResult Invoke()
+        {
+            return View(basket);
+        }
+    }
+}
